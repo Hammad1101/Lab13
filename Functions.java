699d99c0ffb43cd3e16f4f38f580d6fa24e47c34
Hammad1101 <107103133+Hammad1101@users.noreply.github.com>
2022-06-08 00:41:13 -0700

    First comment

---
+++ Functions.java
@@ -0,0 +1,56 @@
+//import java.util.*;
+public class Functions {
+	public static void odd(int x) {
+//		Scanner input = new Scanner(System.in);
+//		x = input.nextInt();
+				if(x<0) {
+			System.out.println("number is odd");
+		}
+		else {
+			System.out.println("wrong number");
+		}
+		
+	}
+	public static void even(int x) {
+//		Scanner input = new Scanner(System.in);
+//		x = input.nextInt();
+				if(x>0) {
+			System.out.println("number is even");
+		}
+		else {
+			System.out.println("wrong number");
+		}
+		
+	}
+	public static void checkPrime(int n) {
+		int i, m=0, flag=0;
+		m=n/2;
+		if(n==0||n==1) {
+			System.out.println(n+ "is not prime number");
+			
+		}
+		else {
+			for(i=2;i<m;i++) {
+				if(n%i==0) {
+					System.out.println(n+"is not prime number");
+					flag=1;
+					break;
+				}
+			}
+			if(flag==0) {
+				System.out.println(n+"is prime number");
+			}}
+			
+		}
+	
+	public static void factorial(int n) {
+		long factorial=1;
+		for(int i=1; i<=n; ++i) {
+			factorial*=i;
+			
+		}
+		System.out.println("Factorial of num"+n+ "is :" + factorial);
+	}
+	
+
+}
---
+++ Odd_test.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Odd_test {
+
+	@Test
+	void test() {
+		assertEquals()
+	}
+
+}
